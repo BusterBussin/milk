b9ac6f09b2e8394b6650dd443106ee8801265f5c
Buster <busterbussin@gmail.com>
2024-11-09 12:50:15 -0500

    milkonthewall
    Created a program that just sings a little song for you. It comes with a check for doubles AND numbers that equal or are below 0.

---
+++ milk.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class milk {
+
+    public void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        double milkDouble;
+        System.out.println("How many bottles of milk are on the wall?");
+        milkDouble = scan.nextDouble();
+        int milk = (int)milkDouble;
+        while (milk <= 0) {
+            System.out.println("There must be atleast one bottle of milk on the wall.");
+            System.out.println("How many bottles of milk are on the wall?");
+            milk = scan.nextInt();
+        }
+        while (milk > 2) {
+            System.out.print(milk + " bottles of milk on the wall, " + milk + " bottles of milk. ");
+            milk = milk - 1;
+            System.out.println("You take one down, pass it around, " + milk + " bottles of milk on the wall.");
+        }
+        if (milk == 2) {
+            System.out.println("2 bottles of milk on the wall, 2 bottles of milk. You take it down, pass it around, one bottle of milk on the wall.");
+            milk = milk - 1;
+        }
+        if (milk == 1) {
+            System.out.println("One bottle of milk on the wall, one bottle of milk. You take it down, pass it around, no more bottles of milk on the wall.");
+        }
+        System.out.println("No more bottles of milk on the wall, no more bottles of milk. You took them around, passed them around, no more bottles of milk on the wall.");
+        System.out.println("Thanks for playing.");
+        scan.close();
+        System.exit(0);
+    
+    }
+}
