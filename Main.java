6f351893e0a383c3863fb8ca03ea6488073bb7f9
Buster <busterbussin@gmail.com>
2024-11-12 12:39:52 -0500

    customizable liquids

--- Main.java
+++ Main.java
@@ -5,28 +5,43 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         double milkDouble;
-        System.out.println("How many bottles of milk are on the wall?");
+        char playAgain = 'Y';
+        String liquid;
+        while(playAgain == 'Y' || playAgain == 'y') {
+        System.out.println("What exactly is on the wall? It should be a liquid, so it makes sense. Won't stop you, though.");
+        liquid = scan.nextLine().toLowerCase();
+        System.out.println("How many bottles of " + liquid + " are on the wall?");
         milkDouble = scan.nextDouble();
         int milk = (int)milkDouble;
+        
         while (milk <= 0) {
-            System.out.println("There must be atleast one bottle of milk on the wall.");
-            System.out.println("How many bottles of milk are on the wall?");
+            System.out.println("There must be atleast one bottle of" + liquid + "on the wall.");
+            System.out.println("How many bottles of " + liquid + " are on the wall?");
             milkDouble = scan.nextDouble();
             milk = (int)milkDouble;
         }
         while (milk > 2) {
-            System.out.print(milk + " bottles of milk on the wall, " + milk + " bottles of milk. ");
+            System.out.print(milk + " bottles of " + liquid + " on the wall, " + milk + " bottles of " + liquid + ". ");
             milk = milk - 1;
-            System.out.println("You take one down, pass it around, " + milk + " bottles of milk on the wall.");
+            System.out.println("You take one down, pass it around, " + milk + " bottles of " + liquid + " on the wall.");
         }
         if (milk == 2) {
-            System.out.println("2 bottles of milk on the wall, 2 bottles of milk. You take one down, pass it around, one bottle of milk on the wall.");
+            System.out.println("2 bottles of " + liquid + " on the wall, 2 bottles of " + liquid + ". You take one down, pass it around, one bottle of " + liquid + "on the wall.");
             milk = milk - 1;
         }
         if (milk == 1) {
-            System.out.println("One bottle of milk on the wall, one bottle of milk. You take it down, pass it around, no more bottles of milk on the wall.");
+            System.out.println("One bottle of " + liquid + "on the wall, one bottle of " + liquid + ". You take it down, pass it around, no more bottles of " + liquid + " on the wall.");
         }
-        System.out.println("No more bottles of milk on the wall, no more bottles of milk. You took them around, passed them around, no more bottles of milk on the wall.");
+        System.out.println("No more bottles of " + liquid + " on the wall, no more bottles of " + liquid + ". You took them around, passed them around, no more bottles of " + liquid + " on the wall.");
+        System.out.println("Do you want to play again? (Y/N)");
+        scan.nextLine();
+        playAgain = scan.nextLine().charAt(0);
+        while (playAgain != 'Y' && playAgain != 'y' && playAgain != 'N' && playAgain != 'n') {
+            System.out.println("That is not a valid choice.");
+            System.out.println("Do you want to play again? (Y/N)");
+            playAgain = scan.nextLine().charAt(0);
+        }
+    }
         System.out.println("Thanks for playing.");
         scan.close();
         System.exit(0);
